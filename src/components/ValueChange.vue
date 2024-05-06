b1ef036931d75d5afdc3a5f6e73f65e8d771c1c8
Richiisontheway <mineiriccardo@gmail.com>
2024-05-06 17:38:52 +0200

    add v-model

--- src/components/ValueChange.vue
+++ src/components/ValueChange.vue
@@ -1,34 +1,40 @@
 <script>
 import { store } from '../store';
+
 export default {
     data() {
         return {
-            store
+            store,
+            money: '1', 
+            money2:'1.08'
         };
     },
     methods: {
-
+        
     }
 };
 </script>
-
 <template>
     <div>
-        <input type="text">
-        <select name="money" id="money">
-            <option :value="elem"
-                v-for="(elem, i) in store.currencyValue" :key="i" :selected = "elem == 'Euro'">
+        <div>
+            <span>
+                {{ money }} equivale a {{ money2 }}
+            </span>
+        </div>
+        <input type="number" v-model="money">
+        <select name="money" id="money" >
+            <option :value="elem" v-for="(elem, i) in store.currencyValue" :key="i" :selected = "elem == 'Euro'">
                 {{ elem }}
             </option>
         </select>
     </div>
     <div>
-        <input type="text">
-        <select name="money" id="money">
-            <option :value="elem"
-                v-for="(elem, i) in store.currencyValue" :key="i" :selected = "elem == 'United States Dollar'">
+        <input type="number"  v-model="money2">
+        <select name="money2" id="money2" >
+            <option :value="elem" v-for="(elem, i) in store.currencyValue" :key="i" :selected = "elem == 'United States Dollar'">
                 {{ elem }}
             </option>
         </select>
     </div>
 </template>
+
