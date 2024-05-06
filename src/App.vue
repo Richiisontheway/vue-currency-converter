66c6206f7898f2227accbdb9400ea9c8302d22f0
Riccardo Minei <147090857+Richiisontheway@users.noreply.github.com>
2024-05-06 10:41:31 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },  
+    methods: {
+
+    },
+    mounted(){
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response)=>{
+            console.log(response)
+        })
+    }
+}
+</script>
+
+<template>
+    <h1>
+        Mia App
+    </h1>
+
+    <AppHeader />
+
+    <AppMain />
+
+    <AppFooter />
+</template>
+
+<style lang="scss">
+@use "assets/scss/main" as *;
+@import "assets/scss/partials/reset";
+</style>
